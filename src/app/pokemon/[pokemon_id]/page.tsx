0cab062189e88acf888f61de4d3c701f2846713b
@Moel32 <moungangivan@gmail.com>
2025-05-27 15:07:31 +0200

    update

--- src/app/pokemon/[pokemon_id]/page.tsx
+++ src/app/pokemon/[pokemon_id]/page.tsx
@@ -1,5 +1,3 @@
-// Instructs React (Next.js) to run this code on the client side.
-// Next.js by default would render this content on the server side where the application is hosted.
 'use client'
 import Pokemon from '@/model/pokemon';
 import { useEffect, useState } from 'react';
@@ -7,51 +5,44 @@ import { Container, Image, Spinner, Row } from 'react-bootstrap';
 import PokemonComponent from './pokemon';
 import PokeNavBar from '@/components/pokeNavBarComp';
 
-// This type is used to get the pokemon id from the url path
 type Params = {
     params: { pokemon_id: string }
 }
 
-// Next.js passes the url parts which are defined between square brackets [] 
-// to the function which renders the page. 
-
-// In our case http://localhost:3000/pokemon/2 is the URL. 
-// Where the 2 is the [pokemon_id] and passed as a parameter.
-export default function PokemonPage({ params }: Params) {
+// Change to async function
+export default async function PokemonPage({ params }: Params) {
     const pokemon_id = params.pokemon_id;
-    //pokemon - A constant state variable which stores the pokemon information and retains the data between renders.
-    //setPokemon - A state setter function to update the variable and trigger React to render the component again.
-    const [pokemon, setPokemon] = useState<Pokemon>();
-    const [isPokemonLoaded, setPokemonLoaded] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const resp = await fetch('/api/pokemon/' + pokemon_id);
-            if (resp.ok) {
-                const pokemon: Pokemon = await resp.json();
-                console.log(pokemon);
-                setPokemon(pokemon);
-            }
-            setPokemonLoaded(true);
-        };
+    // Fetch data on the server side
+    let pokemon: Pokemon | undefined = undefined;
+    let isPokemonLoaded = false;
 
-        fetchData()
-            // Making sure to log errors on the console
-            .catch(error => {
-                console.error(error);
-                setPokemonLoaded(true);
-            });
-    }, []);
+    try {
+        const resp = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/api/pokemon/${pokemon_id}`);
+        if (resp.ok) {
+            pokemon = await resp.json();
+        }
+        isPokemonLoaded = true;
+    } catch (error) {
+        console.error(error);
+        isPokemonLoaded = true;
+    }
 
     return (
         <>
-            <PokeNavBar></PokeNavBar>
+            <PokeNavBar />
             {
-                isPokemonLoaded ?
-                    pokemon ?
-                        <PokemonComponent pokemon={pokemon}></PokemonComponent> :
-                        <Image className='img-fluid mx-auto d-block rounded'
-                            src="https://cdn.dribbble.com/users/2805817/screenshots/13206178/media/6bd36939f8a01d4480cb1e08147e20f3.png" alt="Pokemon"/> :
+                isPokemonLoaded ? (
+                    pokemon ? (
+                        <PokemonComponent pokemon={pokemon} />
+                    ) : (
+                        <Image
+                            className='img-fluid mx-auto d-block rounded'
+                            src="https://cdn.dribbble.com/users/2805817/screenshots/13206178/media/6bd36939f8a01d4480cb1e08147e20f3.png"
+                            alt="Pokemon"
+                        />
+                    )
+                ) : (
                     <Container>
                         <Row className="justify-content-md-center p-2">
                             <Spinner className='p-2' animation='border' role='status' />
@@ -60,6 +51,7 @@ export default function PokemonPage({ params }: Params) {
                             Loading Pokémon...
                         </Row>
                     </Container>
+                )
             }
         </>
     );
